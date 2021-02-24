29006ae464fa8311231ca298ffb7bb9256d14b58
Swackles <Swackles@users.noreply.github.com>
2021-02-24 17:19:33 +0200

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+import morgan from "morgan";
+import express, { Request, Response } from 'express';
+import path from 'path';
+import logger from 'morgan';
+import bodyParser from 'body-parser';
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// catch 404 and forward to error handler
+app.use((req: Request, res: Response) => { res.status(404).send() });
+
+// error handler
+app.use((err: any, req: Request, res: Response) => {
+  if (process.env.NODE_ENV === 'production') {
+    res.status(500).send({
+      error: {
+        code: 'INTERNAL_SERVER_ERROR',
+        message: 'internal server error'
+      }
+    })
+  } else {
+    res.status(err.status || 500).send({
+      error: err,
+      message: err.message
+    })
+  }
+});
+
+app.listen(3000);
+
+export default app;
